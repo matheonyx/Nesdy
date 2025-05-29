673eaa2c400818af689778ec220f15fe0a10b204
matheonyx <133261908+matheonyx@users.noreply.github.com>
2025-05-29 20:34:03 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/Disney++.html',
+        '/2.165ff965.chunk.css',
+        '/app.js',
+        '/icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
